fb02a8d30ec5ad96e0ffe3f435a5bf91f2d1142b
molnartm <molnartm@310-18>
2025-11-24 08:14:34 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Nevek_szűrése
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
